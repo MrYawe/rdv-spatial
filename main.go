31c1faba1894484a42d73f91de132565f2940474
mryawe <yannis.weishaupt@outlook.com>
2016-04-22 22:57:57 +0200

    correction du delta t

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
  // export GOPATH=$HOME/go
  // go get ./...
  // gonum/plot
+ // https://github.com/gonum/plot/wiki/Example-plots
 import(
 	"fmt"
 	"math"
 	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/plot"
+    "github.com/gonum/plot/plotter"
+    "github.com/gonum/plot/plotutil"
+    "github.com/gonum/plot/vg"
 )
+
+
 
 func eulerX(x *mat64.Dense, u *mat64.Dense, A *mat64.Dense, B *mat64.Dense, deltaT float64) *mat64.Dense {
 	res := mat64.NewDense( 4, 1, []float64{0, 0, 0, 0})
@@ -67,37 +74,41 @@ func unPlus1(u *mat64.Dense, deltaJ *mat64.Dense, ro float64) *mat64.Dense {
 	return res
 }
 
-/*
-func test(a mat64.Dense, b mat64.Dense, c mat64.Dense) {
-    var m mat64.Dense
-    m.Mul(&a, &b)
-    fmt.Println(mat64.Formatted(&m))
-
-	var k mat64.Dense
-	k.Add(&b, &c)
-	fmt.Println(mat64.Formatted(&k))
-}*/
+func ToPlotterXYs(denseList []mat64.Dense, w float64, dt float64) plotter.XYs {
+    pts := make(plotter.XYs, len(denseList))
+    for i := range pts {
+		x1 := denseList[i].At(0, 0)
+		x2 := denseList[i].At(2, 0)
+		n := float64(i)
+        pts[i].X = x1*math.Cos(w*n*dt) - x2*math.Sin(w*n*dt) + math.Cos(w*n*dt)
+        pts[i].Y = x1*math.Sin(w*n*dt) + x2*math.Cos(w*n*dt) + math.Sin(w*n*dt)
+    }
+    return pts
+}
 
 func main() {
 
-	u := mat64.NewDense( 2, 1, []float64{0, 0})
 	nbIteration := 500
+	epsilon := 0.001
+	T := 1.0
+	ro := 0.03
+	//deltaT := (3*T)/float64(nbIteration)
+	deltaT := T/float64(nbIteration)
+	fmt.Println(deltaT)
+	w := (2*math.Pi)/T
 
-	for i := 0;  i <= nbIteration; i++ {
+	listeX  := make([]mat64.Dense, nbIteration)
+	u := mat64.NewDense( 2, 1, []float64{0, 0})
 
-		epsilon := 0.001
-		T := 1.0
-		ro := 0.03
-		deltaT := (3*T)/float64(nbIteration)
-	    var w float64 = (2*math.Pi)/T
+	for i := 0;  i < nbIteration; i++ {
+
 	    x := mat64.NewDense( 4, 1, []float64{1, 0, 0, 0})
 	    A := mat64.NewDense(4, 4, []float64{
-	        0, 0, 1, 0,
-	        3*w*w, 0, 0, 2*w,
-	        0, 0, 0, 1,
-	        0, -2*w, 0, 0,
+	        0,     1,    0,    0,
+	        3*w*w, 0,    0,    2*w,
+	        0,     0,    0,    1,
+	        0,    -2*w,  0,    0,
 	    })
-
 	    B := mat64.NewDense(4, 2, []float64{
 	        0, 0,
 	        1, 0,
@@ -106,23 +117,21 @@ func main() {
 	    })
 
 	    // print the output
-	    //fmt.Printf( "%f %f %f\n", z.At(0,0), z.At(1,0)f, z.At(2,0) Af
-	    /*fmt.Printf("w = %f\n\n", w)
+		/*
 	    fmt.Printf("x = %0.4v\n\n", mat64.Formatted(x, mat64.Prefix("    ")))
 	    fmt.Printf("u = %0.4v\n\n", mat64.Formatted(u, mat64.Prefix("    ")))
 	    fmt.Printf("A = %0.4v\n\n", mat64.Formatted(A, mat64.Prefix("    ")))
 	    fmt.Printf("B = %0.4v\n\n", mat64.Formatted(B, mat64.Prefix("    ")))*/
 
-	    for i := 0; i <= nbIteration; i++ {
+	    for i := 0; i < nbIteration; i++ {
 			x = eulerX(x, u, A, B, deltaT)
-			//fmt.Printf("x(%d) = %0.4v\n\n", i, mat64.Formatted(x, mat64.Prefix("         ")))
 	    }
+		listeX[i] = *x
 		//fmt.Printf("x(%d) = %0.4v\n\n", nbIteration, mat64.Formatted(x, mat64.Prefix("         ")))
 
 		P := x
 		for i := nbIteration-1; i >= 0; i-- {
 			P = eulerRetroP(P, A, deltaT, int(T))
-			//fmt.Printf("P(%d) = %0.4v\n\n", i, mat64.Formatted(P, mat64.Prefix("         ")))
 	    }
 		//fmt.Printf("P(%d) = %0.4v\n\n", 0, mat64.Formatted(P, mat64.Prefix("       ")))
 
@@ -130,35 +139,28 @@ func main() {
 		//fmt.Printf("deltaJ = %0.4v\n\n", mat64.Formatted(deltaJ, mat64.Prefix("       ")))
 
 		u = unPlus1(u, deltaJ, ro)
-		fmt.Printf("u(%d) = %0.4v\n\n", i, mat64.Formatted(u, mat64.Prefix("         ")))
+		//fmt.Printf("u(%d) = %0.4v\n\n", i, mat64.Formatted(u, mat64.Prefix("         ")))
 	}
 	//fmt.Printf("u(%d) = %0.4v\n\n", nbIteration, mat64.Formatted(u, mat64.Prefix("       ")))
 
 
+	/** Affichage **/
+	p, err := plot.New()
+	if err != nil {
+	   panic(err)
+	}
 
+	p.Title.Text = "Rendez-vous spatial"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
 
+	err = plotutil.AddLinePoints(p, "x", ToPlotterXYs(listeX, w, deltaT))
+	if err != nil {
+	   panic(err)
+	}
 
-	/*
-    b := mat64.NewDense(4, 3, []float64{
-        1, 2, 3,
-        4, 5, 6,
-        7, 8, 9,
-        10, 11, 12,
-    })
-	c := mat64.NewDense(4, 3, []float64{
-        1, 2, 3,
-        4, 5, 6,
-        7, 8, 9,
-        10, 11, 12,
-    })
-
-    var m mat64.Dense
-    m.Mul(A, x)
-    fmt.Println(mat64.Formatted(&m))
-
-	var k mat64.Dense
-	k.Add(b, c)
-	fmt.Println(mat64.Formatted(&k))
-	*/
-
+	// Save the plot to a PNG file.
+	if err := p.Save(7*vg.Inch, 7*vg.Inch, "points2.png"); err != nil {
+	   panic(err)
+	}
 }
